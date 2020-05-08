9ed0070cf5def9dde31d986f8d24fe1cf3e7797f
Anthony Otha <yahanthony8@gmail.com>
2020-05-08 20:55:34 +0200

    mise à jour

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,56 @@
-self.addEventListener('install', event=> {
+self.addEventListener('install', event => {
     self.skipWaiting()
-    console.log('service worker installer')
+    console.log('worker installed', event)
 })
-self.addEventListener('activate', event=> {
-    console.log('service worker activé')
+
+self.addEventListener('activate', event => {
+    console.log('worker activated', event)
 })
-self.addEventListener('push', event=> {
-    console.log('notification reçu ', event)
-   // const data = event.data.json()
-    // const options= {
+
+self.addEventListener('push', event => {
+    console.log('push fired', event)
+
+    const data = []
+
+    // const options = {
     //     body: data.body,
-    //     icon: data.icon?data.icon: '',
-    //     image: data.image?data.image:''
+    //     icon: data.icon ? data.icon : '',
+    //     image: data.image ? data.image : '',
+    //     data: {
+    //         url: data.url
+    //     }
     // }
+
+    if (data.actions)
+        options.actions = data.actions
+
     event.waitUntil(
-            Promise.all([
-                self.registration.showNotification("salut")])
-        )
+        Promise.all([
+            self.registration.showNotification("slt")
+        ])
+    )
 })
-self.addEventListener('notificationclick', event=> {
-    console.log('notification cliqué ', event)
+
+self.addEventListener('notificationclick', event => {
+    console.log('notificationclick fired', event)
+        // INFO: You can customize this callback for your needs
+
+    // currently we are closing notification on click
     event.notification.close()
-    event.notification.data.url= 'https://yahshoua.github.io/serviceworker.github.io/' //url de votre site
-    var clickResponsePromise = Promise.resolve()
-    if(event.notification.data.url) {
-        clickResponsePromise = clients.openWindow(event.notification.data.url)
-        event.waitingUntil(
-            Promise.all([
-                clickResponsePromise
-                ]))
+
+    if (event.action) {
+        console.log('action clicked', event.action)
     }
+
+    // then if url is provided open a new window
+    var clickResponsePromise = Promise.resolve()
+    if (event.notification.data && event.notification.data.url) {
+        clickResponsePromise = clients.openWindow(event.notification.data.url)
+    }
+
+    event.waitUntil(
+        Promise.all([
+            clickResponsePromise
+        ])
+    )
 })
