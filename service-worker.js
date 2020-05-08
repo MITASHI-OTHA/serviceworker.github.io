2bd5d8657d71d6d7442d1d041320e7b396cecbb0
Anthony Otha <yahanthony8@gmail.com>
2020-05-08 17:53:34 +0200

    mise à jour

--- service-worker.js
+++ service-worker.js
@@ -1,56 +1,20 @@
-self.addEventListener('install', event => {
+self.addEventListerner('install', event=> {
     self.skipWaiting()
-    console.log('worker installed', event)
+    console.log('service worker installer')
 })
-
-self.addEventListener('activate', event => {
-    console.log('worker activated', event)
+self.addEventListerner('activate', event=> {
+    console.log('service worker activé')
 })
-
-self.addEventListener('push', event => {
-    console.log('push fired', event)
-
+self.addEventListerner('push', event=> {
+    console.log('notification reçu ', event)
     const data = event.data.json()
-
-    const options = {
+    const options= {
         body: data.body,
-        icon: data.icon ? data.icon : '',
-        image: data.image ? data.image : '',
-        data: {
-            url: data.url
-        }
+        icon: data.icon?data.icon: '',
+        image: data.image?data.image:''
     }
-
-    if (data.actions)
-        options.actions = data.actions
-
-    event.waitUntil(
-        Promise.all([
-            self.registration.showNotification(data.title, options)
-        ])
-    )
-})
-
-self.addEventListener('notificationclick', event => {
-    console.log('notificationclick fired', event)
-        // INFO: You can customize this callback for your needs
-
-    // currently we are closing notification on click
-    event.notification.close()
-
-    if (event.action) {
-        console.log('action clicked', event.action)
-    }
-
-    // then if url is provided open a new window
-    var clickResponsePromise = Promise.resolve()
-    if (event.notification.data && event.notification.data.url) {
-        clickResponsePromise = clients.openWindow(event.notification.data.url)
-    }
-
-    event.waitUntil(
-        Promise.all([
-            clickResponsePromise
-        ])
-    )
+    event.waitingUntil(
+            Promise.all([
+                self.registration.showNotification(data.title, options)])
+        )
 })
